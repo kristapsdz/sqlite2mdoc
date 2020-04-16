4bde85d5f336bedc7026f74212bdc236352af22e
kristapsdz <kristaps@bsd.lv>
2020-04-16 08:16:06 +0000

    Newest oconfigure.

--- tests.c
+++ tests.c
@@ -39,6 +39,42 @@ main(void)
 	return(0);
 }
 #endif /* TEST_CAPSICUM */
+#if TEST_CRYPT
+#if defined(__linux__)
+# define _GNU_SOURCE /* old glibc */
+# define _DEFAULT_SOURCE /* new glibc */
+#endif
+#if defined(__sun)
+# ifndef _XOPEN_SOURCE /* SunOS already defines */
+#  define _XOPEN_SOURCE /* XPGx */
+# endif
+# define _XOPEN_SOURCE_EXTENDED 1 /* XPG4v2 */
+# ifndef __EXTENSIONS__ /* SunOS already defines */
+#  define __EXTENSIONS__ /* reallocarray, etc. */
+# endif
+#endif
+#include <unistd.h>
+
+int main(void)
+{
+	char	*v;
+
+	v = crypt("this_is_a_key", "123455");
+	return v == NULL;
+}
+#endif /* TEST_CRYPT */
+#if TEST_ENDIAN_H
+#ifdef __linux__
+# define _DEFAULT_SOURCE
+#endif
+#include <endian.h>
+
+int
+main(void)
+{
+	return !htole32(23);
+}
+#endif /* TEST_ENDIAN_H */
 #if TEST_ERR
 /*
  * Copyright (c) 2015 Ingo Schwarze <schwarze@openbsd.org>
@@ -80,6 +116,53 @@ main(void)
 	return(0);
 }
 #endif /* TEST_EXPLICIT_BZERO */
+#if TEST_FTS
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fts.h>
+
+int
+main(void)
+{
+	const char	*argv[2];
+	FTS		*ftsp;
+	FTSENT		*entry;
+
+	argv[0] = ".";
+	argv[1] = (char *)NULL;
+
+	ftsp = fts_open((char * const *)argv,
+	    FTS_PHYSICAL | FTS_NOCHDIR, NULL);
+
+	if (ftsp == NULL)
+		return 1;
+
+	entry = fts_read(ftsp);
+
+	if (entry == NULL)
+		return 1;
+
+	if (fts_set(ftsp, entry, FTS_SKIP) != 0) 
+		return 1;
+
+	if (fts_close(ftsp) != 0)
+		return 1;
+
+	return 0;
+}
+#endif /* TEST_FTS */
+#if TEST_GETEXECNAME
+#include <stdlib.h>
+
+int
+main(void)
+{
+	const char * progname;
+
+	progname = getexecname();
+	return progname == NULL;
+}
+#endif /* TEST_GETEXECNAME */
 #if TEST_GETPROGNAME
 #include <stdlib.h>
 
@@ -107,6 +190,18 @@ main(void)
 	return 0;
 }
 #endif /* TEST_INFTIM */
+#if TEST_LIB_SOCKET
+#include <sys/socket.h>
+
+int
+main(void)
+{
+	int fds[2], c;
+
+	c = socketpair(AF_UNIX, SOCK_STREAM, 0, fds);
+	return c == -1;
+}
+#endif /* TEST_LIB_SOCKET */
 #if TEST_MD5
 #include <sys/types.h>
 #include <md5.h>
@@ -114,9 +209,11 @@ main(void)
 int main(void)
 {
 	MD5_CTX ctx;
+	char result[MD5_DIGEST_STRING_LENGTH];
 
 	MD5Init(&ctx);
-	MD5Update(&ctx, "abcd", 4);
+	MD5Update(&ctx, (const unsigned char *)"abcd", 4);
+	MD5End(&ctx, result);
 
 	return 0;
 }
@@ -158,6 +255,33 @@ int main(void)
 	return 0;
 }
 #endif /* TEST_MEMSET_S */
+#if TEST_MKFIFOAT
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(void) {
+	mkfifoat(AT_FDCWD, "this/path/should/not/exist", 0600);
+	return 0;
+}
+#endif /* TEST_MKFIFOAT */
+#if TEST_MKNODAT
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(void) {
+	mknodat(AT_FDCWD, "this/path/should/not/exist", S_IFIFO | 0600, 0);
+	return 0;
+}
+#endif /* TEST_MKNODAT */
+#if TEST_OSBYTEORDER_H
+#include <libkern/OSByteOrder.h>
+
+int
+main(void)
+{
+	return !OSSwapHostToLittleInt32(23);
+}
+#endif /* TEST_OSBYTEORDER_H */
 #if TEST_PATH_MAX
 /*
  * POSIX allows PATH_MAX to not be defined, see
@@ -210,7 +334,20 @@ main(void)
 	return !program_invocation_short_name;
 }
 #endif /* TEST_PROGRAM_INVOCATION_SHORT_NAME */
+#if TEST_READPASSPHRASE
+#include <stddef.h>
+#include <readpassphrase.h>
+
+int
+main(void)
+{
+	return !!readpassphrase("prompt: ", NULL, 0, 0);
+}
+#endif /* TEST_READPASSPHRASE */
 #if TEST_REALLOCARRAY
+#ifdef __NetBSD__
+# define _OPENBSD_SOURCE
+#endif
 #include <stdlib.h>
 
 int
@@ -256,6 +393,44 @@ main(void)
 	return(EFAULT == errno ? 0 : 1);
 }
 #endif /* TEST_SECCOMP_FILTER */
+#if TEST_SETRESGID
+#define _GNU_SOURCE /* linux */
+#include <sys/types.h>
+#include <unistd.h>
+
+int
+main(void)
+{
+	return setresgid(-1, -1, -1) == -1;
+}
+#endif /* TEST_SETRESGID */
+#if TEST_SETRESUID
+#define _GNU_SOURCE /* linux */
+#include <sys/types.h>
+#include <unistd.h>
+
+int
+main(void)
+{
+	return setresuid(-1, -1, -1) == -1;
+}
+#endif /* TEST_SETRESUID */
+#if TEST_SHA2_H
+#include <sys/types.h>
+#include <sha2.h>
+
+int main(void)
+{
+	SHA2_CTX ctx;
+	char result[SHA256_DIGEST_STRING_LENGTH];
+
+	SHA256Init(&ctx);
+	SHA256Update(&ctx, (const unsigned char *)"abcd", 4);
+	SHA256End(&ctx, result);
+
+	return 0;
+}
+#endif /* TEST_SHA2_H */
 #if TEST_SOCK_NONBLOCK
 /*
  * Linux doesn't (always?) have this.
@@ -271,6 +446,13 @@ main(void)
 	return 0;
 }
 #endif /* TEST_SOCK_NONBLOCK */
+#if TEST_STATIC
+int
+main(void)
+{
+	return 0; /* not meant to do anything */
+}
+#endif /* TEST_STATIC */
 #if TEST_STRLCAT
 #include <string.h>
 
@@ -335,7 +517,9 @@ main(void)
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
-
+#ifdef __NetBSD__
+# define _OPENBSD_SOURCE
+#endif
 #include <stdlib.h>
 
 int
@@ -362,32 +546,130 @@ main(void)
 	return 0;
 }
 #endif /* TEST_STRTONUM */
-#if TEST_SYSTRACE
-#include <sys/param.h>
-#include <dev/systrace.h>
+#if TEST_SYS_BYTEORDER_H
+#include <sys/byteorder.h>
 
+int
+main(void)
+{
+	return !LE_32(23);
+}
+#endif /* TEST_SYS_BYTEORDER_H */
+#if TEST_SYS_ENDIAN_H
+#include <sys/endian.h>
+
+int
+main(void)
+{
+	return !htole32(23);
+}
+#endif /* TEST_SYS_ENDIAN_H */
+#if TEST_SYS_MKDEV_H
+#include <sys/types.h>
+#include <sys/mkdev.h>
+
+int
+main(void)
+{
+	return !minor(0);
+}
+#endif /* TEST_SYS_MKDEV_H */
+#if TEST_SYS_QUEUE
+#include <sys/queue.h>
+#include <stddef.h>
+
+struct foo {
+	int bar;
+	TAILQ_ENTRY(foo) entries;
+};
+
+TAILQ_HEAD(fooq, foo);
+
+int
+main(void)
+{
+	struct fooq foo_q;
+	struct foo *p, *tmp;
+	int i = 0;
+
+	TAILQ_INIT(&foo_q);
+
+	/*
+	 * Use TAILQ_FOREACH_SAFE because some systems (e.g., Linux)
+	 * have TAILQ_FOREACH but not the safe variant.
+	 */
+
+	TAILQ_FOREACH_SAFE(p, &foo_q, entries, tmp)
+		p->bar = i++;
+	return 0;
+}
+#endif /* TEST_SYS_QUEUE */
+#if TEST_SYS_SYSMACROS_H
+#include <sys/sysmacros.h>
+
+int
+main(void)
+{
+	return !minor(0);
+}
+#endif /* TEST_SYS_SYSMACROS_H */
+#if TEST_SYS_TREE
+#include <sys/tree.h>
 #include <stdlib.h>
 
+struct node {
+	RB_ENTRY(node) entry;
+	int i;
+};
+
+static int
+intcmp(struct node *e1, struct node *e2)
+{
+	return (e1->i < e2->i ? -1 : e1->i > e2->i);
+}
+
+RB_HEAD(inttree, node) head = RB_INITIALIZER(&head);
+RB_PROTOTYPE(inttree, node, entry, intcmp)
+RB_GENERATE(inttree, node, entry, intcmp)
+
+int testdata[] = {
+	20, 16, 17, 13, 3, 6, 1, 8, 2, 4
+};
+
 int
 main(void)
 {
+	size_t i;
+	struct node *n;
 
-	return(0);
+	for (i = 0; i < sizeof(testdata) / sizeof(testdata[0]); i++) {
+		if ((n = malloc(sizeof(struct node))) == NULL)
+			return 1;
+		n->i = testdata[i];
+		RB_INSERT(inttree, &head, n);
+	}
+
+	return 0;
 }
-#endif /* TEST_SYSTRACE */
-#if TEST_ZLIB
-#include <stddef.h>
-#include <zlib.h>
+
+#endif /* TEST_SYS_TREE */
+#if TEST_UNVEIL
+#include <unistd.h>
 
 int
 main(void)
 {
-	gzFile		 gz;
-
-	if (NULL == (gz = gzopen("/dev/null", "w")))
-		return(1);
-	gzputs(gz, "foo");
-	gzclose(gz);
-	return(0);
+	return -1 != unveil(NULL, NULL);
 }
-#endif /* TEST_ZLIB */
+#endif /* TEST_UNVEIL */
+#if TEST_WAIT_ANY
+#include <sys/wait.h>
+
+int
+main(void)
+{
+	int st;
+
+	return waitpid(WAIT_ANY, &st, WNOHANG) != -1;
+}
+#endif /* TEST_WAIT_ANY */
